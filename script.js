7339c9faa24f64eb403b31b344baa2df9cd964ef
Michał <KrzyzowskiM1998@gmail.com>
2022-02-19 12:05:48 +0100

    Added basic element of DOM to use leater as variables in script

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+//const openModalBtns = document.querySelector('show-modal'); //return only first element selected
+const openModalBtns = document.querySelectorAll('.show-modal'); //return node list
+
+for (let i = 0; i < openModalBtns.length; i++) {
+  console.log(openModalBtns[i]);
+}
